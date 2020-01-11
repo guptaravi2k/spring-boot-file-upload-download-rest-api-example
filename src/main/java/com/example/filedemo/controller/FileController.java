47c4a3ee09d5e8b757788c5ae5c0229130ff3f8d
Ravi Gupta <guptaravi2k@gmail.com>
2020-01-12 09:29:14 +1100

    added cross origin support and changed local file save path

--- src/main/java/com/example/filedemo/controller/FileController.java
+++ src/main/java/com/example/filedemo/controller/FileController.java
@@ -19,6 +19,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class FileController {
 
